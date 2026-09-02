1a710f8dc9c3941a78bca239e5ec222818433369
aleister1102 <marucube35@gmail.com>
2026-09-03 01:52:44 +0700

    feat: handle colorization for text/json MIME type (#26)

--- caido.config.ts
+++ caido.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   id: "jq",
   name: "JQ",
   description: "JQ view mode for JSON bodies",
-  version: "1.5.0",
+  version: "1.6.0",
   author: {
     name: "insomnia1102",
     email: "marucube35@gmail.com",
